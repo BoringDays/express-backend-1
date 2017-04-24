cf5f2ff8d4989c419fc3827b7bbb8e1ccab1f292
Matt Leung <Matt Leung>
2017-04-24 18:11:11 +0800

    add new CRUD methods

--- server/utils/index.js
+++ server/utils/index.js
@@ -19,41 +19,22 @@ const findList = (req, res, Model, sort = '-_id') => {
   Promise.all([
     Model.find().sort(sort).skip(start).limit(limit).exec(),
     Model.count()
-  ]).then(result => {
-    const total = result[1]
-    const totalPage = Math.ceil(total / limit)
-
-    res.json({
-      code: 200,
-      data: {
-        list: result[0],
-        total,
-        hasNext: totalPage > page ? 1 : 0,
-        hasPrev: page > 1 ? 1 : 0
-      }
-    })
-  }).catch(err => {
-    res.json({
-      code: 500,
-      errMsg: '' + err
-    })
-  })
-}
-
-/**
- * 查询单条数据
- * @param req {Object} 请求
- * @param res {Object} 返回
- * @param Model {Model} 查询的model
- */
-const findData = (req, res, Model) => {
-  Model.findOne(req.params).exec()
+  ])
     .then(result => {
+      const total = result[1]
+      const totalPage = Math.ceil(total / limit)
+
       res.json({
         code: 200,
-        data: result
+        data: {
+          list: result[0],
+          total,
+          hasNext: totalPage > page ? 1 : 0,
+          hasPrev: page > 1 ? 1 : 0
+        }
       })
-    }).catch(err => {
+    })
+    .catch(err => {
       res.json({
         code: 500,
         errMsg: '' + err
@@ -62,36 +43,97 @@ const findData = (req, res, Model) => {
 }
 
 /**
- * 保存数据
- * @param req {Object} 请求
+ * 查询单条数据
+ * @param data {Object} 需要查询的数据
+ * @param res {Object} 返回
+ * @param Model {Model} 查询的model
+ */
+const findData = (data, res, Model) => {
+  Model.findOne(data).exec()
+    .then(result => {
+      res.json({
+        code: 200,
+        data: result
+      })
+    })
+    .catch(err => {
+      res.json({
+        code: 500,
+        errMsg: '' + err
+      })
+    })
+}
+
+/**
+ * 插入数据
+ * @param data {Object} 需要插入的新数据
  * @param res {Object} 返回
  * @param Model {Model} 需要保存的model
  */
-const saveData = (req, res, Model) => {
+const insertData = (data, res, Model) => {
+  Model.insert(data).exec()
+    .then(() => {
+      res.json({
+        code: 200,
+        data: 'insert succeed!'
+      })
+    })
+    .catch(err => {
+      res.json({
+        code: 500,
+        errMsg: '' + err
+      })
+    })
 }
 
 /**
  * 更新数据
- * @param req {Object} 请求
+ * @param data {Object} 需要更新的数据
  * @param res {Object} 返回
  * @param Model {Model} 需要更新的model
  */
-const updateData = (req, res, Model) => {
+const updateData = (data, res, Model) => {
+  Model.update({_id: data.id}, data).exec()
+    .then(() => {
+      res.json({
+        code: 200,
+        data: 'update succeed!'
+      })
+    })
+    .catch(err => {
+      res.json({
+        code: 500,
+        errMsg: '' + err
+      })
+    })
 }
 
 /**
  * 删除数据
- * @param req {Object} 请求
+ * @param data {Object} 需要删除的数据
  * @param res {Object} 返回
  * @param Model {Model} 需要执行删除操作的model
  */
-const deleteData = (req, res, Model) => {
+const deleteData = (data, res, Model) => {
+  Model.remove({_id: data.id}, data).exec()
+    .then(() => {
+      res.json({
+        code: 200,
+        data: 'delete succeed!'
+      })
+    })
+    .catch(err => {
+      res.json({
+        code: 500,
+        errMsg: '' + err
+      })
+    })
 }
 
 export {
   findList,
   findData,
-  saveData,
+  insertData,
   updateData,
   deleteData
 }
