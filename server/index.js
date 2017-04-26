e5308485c49eb3db5e17e4b33b60cd96b8f8e79a
Matt Leung <Matt Leung>
2017-04-26 18:08:30 +0800

    fixed sth about babel

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import logger from 'morgan'
 
 import routes from './routes/index'
 
+// 已知目前node的机制无法在chrome dev tools里面正常应用sourceMaps，只能调试编译后的代码；新版的node和Chrome有报告说问题已经修复
+// 想好好玩async请去web端或者node > 7.6，能用上10月份的node 8就更好了
+// https://github.com/nodejs/node/issues/8369
+
 const app = express()
 // const upload = multer()
 const port = 3000
